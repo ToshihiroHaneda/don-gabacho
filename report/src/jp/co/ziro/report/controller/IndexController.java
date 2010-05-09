b4ee5fc3eaf9c3c7d37e8000e18be3e1067b1f32
secondarykey <secondarykey@gmail.com>
2010-05-09 17:03:09 +0000

    コメント打ち

--- report/src/jp/co/ziro/report/controller/IndexController.java
+++ report/src/jp/co/ziro/report/controller/IndexController.java
@@ -3,6 +3,10 @@ package jp.co.ziro.report.controller;
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
+/**
+ * メニューの表示
+ * @author z001
+ */
 public class IndexController extends Controller {
 
     @Override
